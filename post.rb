cab72107dcacd2aaebc5d7463aead83398a45dd4
Yury Kornelyuk <zlyucher@gmail.com>
2022-09-26 17:22:29 +0200

    Notepad v.2 With SQLite support

--- post.rb
+++ post.rb
@@ -1,42 +1,71 @@
 # encoding: utf-8
-#
+
+require 'sqlite3'
+
 # Базовый класс «Запись» — здесь мы определим основные методы и свойства,
 # общие для всех типов записей.
 class Post
-  # Метод post_types класса Post, возвращает всех известных ему детей класса
-  # Post в виде массива классов.
-  #
-  # Обратите внимание, что класс, объявленный с self. перед название — это не
-  # метод экземпляра класса, а метод самого класса (их ещё часто называют
-  # статические методы).
+  SQLITE_DB_FILE = 'notepad.db'.freeze
+
   def self.post_types
-    [Memo, Task, Link]
+    { 'Memo' => Memo, 'Task' => Task, 'Link' => Link }
   end
 
-  # Строго говоря этот метод self.types не очень хорош — код родительского
-  # класса в идеале не должен никак зависеть от того, какие у него дети. Мы его
-  # использовали для простоты (адекватно поставленной задаче).
-  #
-  # В сложных приложениях это делается немного иначе: например отдельный класс
-  # владеет всей информацией, и умеет создавать нужные объекты (т. н. шаблон
-  # проектирования «Фабрика»).
-  #
-  # Или каждый дочерний класс динамически регистрируется в подобном массиве
-  # сам во время загрузки программы.
-  #
-  # Подробнее об этом вы можете прочитать в книгах о паттернах проектирования,
-  # ссылки на которых приведены в дополнительных материалах.
-
-  # Метод create класса Post динамически (в зависимости от параметра) создает
-  # объект нужного класса (Memo, Task или Link) из набора возможных детей,
-  # получая список с помощью метода post_types, объявленного выше.
-  def self.create(type_index)
-    post_types[type_index].new
+  def self.create(type)
+    post_types[type].new
   end
 
   def initialize
     @created_at = Time.now
     @text = []
+  end
+
+  # Метод класса find_by_id находит в базе запись по идентификатору
+  def self.find_by_id(id)
+    # Если id не передали, мы ничего не ищем, а возвращаем nil
+    return if id.nil?
+
+    # Если id передали, едем дальше
+    db = SQLite3::Database.open(SQLITE_DB_FILE)
+    db.results_as_hash = true
+    result = db.execute('SELECT * FROM posts WHERE  row_id = ?', id)
+    db.close
+
+    # Если в результате запроса получили пустой массив, снова возвращаем nil
+    return nil if result.empty?
+
+    # Если результат не пуст, едем дальше
+    result = result[0]
+
+    # Создаем пост нужного типа, заполняем его данными и возвращаем
+    post = create(result['type'])
+    post.load_data(result)
+    post
+  end
+
+  # Метод класса find_all возвращает массив записей из базы данных, который
+  # можно например показать в виде таблицы на экране.
+  def self.find_all(limit, type)
+    db = SQLite3::Database.open(SQLITE_DB_FILE)
+
+    db.results_as_hash = false
+
+    query = 'SELECT * FROM posts '
+    query += 'WHERE type = :type ' unless type.nil?
+    query += 'ORDER by row_id DESC '
+    query += 'LIMIT :limit ' unless limit.nil?
+
+    statement = db.prepare query
+
+    statement.bind_param('type', type) unless type.nil?
+    statement.bind_param('limit', limit) unless limit.nil?
+
+    result = statement.execute!
+
+    statement.close
+    db.close
+
+    result
   end
 
   def read_from_console
@@ -47,8 +76,38 @@ class Post
     # Этот метод должен быть реализован у каждого ребенка
   end
 
+  def load_data(data_hash)
+    @created_at = Time.parse(data_hash['created_at'])
+    @text = data_hash['text']
+  end
+
+  def to_db_hash
+    {
+      'type' => self.class.name,
+      'created_at' => @created_at.to_s
+    }
+  end
+
+  def save_to_db
+    db = SQLite3::Database.open(SQLITE_DB_FILE)
+    db.results_as_hash = true
+
+    post_hash = to_db_hash
+
+    db.execute(
+      'INSERT INTO posts (' +
+        post_hash.keys.join(', ') +
+        ") VALUES (#{('?,' * post_hash.size).chomp(',')})",
+      post_hash.values
+    )
+
+    insert_row_id = db.last_insert_row_id
+    db.close
+    insert_row_id
+  end
+
   def save
-    file = File.new(file_path, 'w:UTF-8') # открываем файл на запись
+    file = File.new(file_path, 'w:UTF-8')
 
     to_strings.each { |string| file.puts(string) }
 
